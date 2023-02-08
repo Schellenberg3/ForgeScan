fa8a15b88bc526d7ac19946791d199029cbaeb1f
Schellenberg3 <schellenberg.andrew@gmail.com>
2023-02-07 21:04:08 -0500

    Add tools for vector memory usage analysis

---
+++ include/point_gen/memory_utils.h
@@ -0,0 +1,40 @@
+#ifndef MY_MEMORY_UTILS_H
+#define MY_MEMORY_UTILS_H
+
+#include <cstddef>
+#include <vector>
+
+// Conversion utility for bits_to_megabytes. 1e-6
+#define MB_PER_BYTE 0.000001
+
+
+/// @brief Templated function to find the size of a vector in bytes
+/// @param vec Vector to find the size of.
+/// @return Size in bytes of memory in the vector is currently using
+template<typename T>
+size_t const inline vector_size(const typename std::vector<T>& vec)
+{
+    return sizeof(T) * vec.size();
+}
+
+
+/// @brief Templated function to find the capacity of a vector in bytes
+/// @param vec Vector to find the capacity of.
+/// @return Size in bytes of memory in the vector has capacity for.
+template<typename T>
+size_t const inline vector_capacity(const typename std::vector<T>& vec)
+{
+    return sizeof(T) * vec.capacity();  // Note 
+}
+
+
+/// @brief Converts bytes to megabytes (MB).
+/// @param bytes Number of bytes in memory.
+/// @return Memory size converted to MB.
+double const inline byte_to_megabytes(const size_t& bytes)
+{
+    return bytes * MB_PER_BYTE;
+}
+
+
+#endif // MY_MEMORY_UTILS_H

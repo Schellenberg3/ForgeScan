6e10f409b1a04623878b408acb5fece2f004cd0f
Andrew Schellenberg <schellenberg.andrew@gmail.com>
2023-03-28 11:14:37 -0400

    Update test traversal to profile methods

--- src/test_traversal.cpp
+++ src/test_traversal.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
+#include <random>
+
+#include <Eigen/Dense>
 
 #include <ForgeScan/voxel_grid.h>
 #include <ForgeScan/grid_processor.h>
 #include <ForgeScan/grid_traversal.h>
+#include <ForgeScan/timing_utils.h>
 
 
 /// @brief Simple script for manually adding points to a VoxelGrid within [-1,-1,-1] and [+1,+1,+1].
@@ -13,72 +17,104 @@ int main(int argc, char** argv)
     Eigen::Vector3d lower(-1.0, -1.0, -1.0), upper(1.0, 1.0, 1.0);
     double res = 0.02;
 
-    VoxelGrid grid(res, lower, upper, false);  // 2m x 2m x 2m cube with 0.02 m resolution
-    std::cout << "Initialized the VoxelGrid!" << std::endl;
+    VoxelGrid grid_linear(res, lower, upper, false);  // 2m x 2m x 2m cube with 0.02 m resolution
+    VoxelGrid grid_approx(res, lower, upper, false);
+    VoxelGrid grid_exact(res, lower, upper, false);
+    std::cout << "Initialized each VoxelGrid!" << std::endl;
 
-    voxel_distance d_ls = 1;
-    VoxelElementUpdate update_ls(&d_ls);
+    voxel_distance distance = 1;
+    VoxelElementUpdate voxel_update(&distance);
 
-    voxel_distance d_la = 10;
-    VoxelElementUpdate update_la(&d_la);
+    int num = argc > 1 ? std::stoi(argv[1]) : 5000;
+    std::vector<Vector3d> start_vecs, end_vecs;
+    start_vecs.reserve(num);
+    end_vecs.reserve(num);
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dist(-0.9, 0.9);
+        
+        for (int i = 0; i < num; ++i)
+        {
+            start_vecs.push_back(Vector3d(dist(gen), dist(gen), dist(gen)));
+            end_vecs.push_back(Vector3d(dist(gen), dist(gen), dist(gen)));
+        }
 
-    voxel_distance d_le = 20;
-    VoxelElementUpdate update_le(&d_le);
+        end_vecs[0] = start_vecs[0];
+        for (int i = 0; i < 10; ++i)
+        {
+            std::cout << "["<<i<<"] " << start_vecs[i].transpose() << " -> " << end_vecs[i].transpose() << std::endl; 
+        }
+    }
+    std::cout << "Generated " << num << " random lines to add with each method." << std::endl;
 
-    point rs = Vector3d(-0.8, -0.7, -0.6);
-    point re = Vector3d(0.1, 0.1, 0.1);
+    SimpleTimer t_lin, t_apr, t_exa;
 
     // Linear Space
     if (true)
     {
-        std::cout << "Adding linear spaced ray" << std::endl;
-        point rs_ls = rs;
-        point re_ls = re;
-        addRayLinspace(
-            grid, update_ls,
-            rs_ls, re_ls,
-            60
-        );
+        t_lin.start();
+        for (int i = 0; i < num; ++i)
+            {
+            addRayLinspace(
+                grid_linear, voxel_update,
+                start_vecs[i], end_vecs[i],
+                60
+            );
+        }
+        t_lin.stop();
+        std::cout << "Added " << num << " with linear method" << std::endl;
     }
 
     // Approx Line
     if (true)
     {
-        std::cout << "Adding approximate ray" << std::endl;
-        point rs_la = rs;
-        point re_la = re;
-        for (int i = 0; i < 1; ++i)
-        {
-            //rs_la[i] += 0.15;
-            //re_la[i] += 0.15;
+        float rr = 0.9;
+        t_apr.start();
+        for (int i = 0; i < num; ++i)
+            {
+            addRayApprox(
+                grid_approx, voxel_update,
+                start_vecs[i], end_vecs[i],
+                rr
+            );
         }
-        addRayApprox(
-            grid, update_la,
-            rs_la, re_la,
-            0.9
-        );
+        t_apr.stop();
+        std::cout << "Added " << num << " with approximate method. Resolution parameter rr="
+                  << rr << std::endl;
     }
 
     // Exact Line
     if (true)
     {
-        std::cout << "Adding exact ray" << std::endl;
-        point rs_le = rs;
-        point re_le = re;
-        for (int i = 0; i < 1; ++i)
-        {
-            //rs_le[i] += 0.3;
-            //r_e_le[i] += 0.3;
+        t_exa.start();
+        for (int i = 0; i < num; ++i)
+            {
+            addRayExact(
+                grid_exact, voxel_update,
+                start_vecs[i], end_vecs[i], 
+                0.0, 1.0, [](const grid_idx & gidx){}
+            );
         }
-        addRayExact(
-            grid, update_le,
-            rs_le, re_le,
-            0, 1
-        );
+        t_exa.stop();
+        std::cout << "Added " << num << " with exact method" << std::endl;
     }
 
-    std::cout << "Saving to CSV..."  << std::endl;
-    grid.save_csv("test_points.csv");
+    std::cout << "Run times in ms were:\n\tLinear\n\t\t" << t_lin.elapsedMilliseconds()
+              << "\n\tApprox.\n\t\t" << t_apr.elapsedMilliseconds()
+              << "\n\tExact\n\t\t" << t_exa.elapsedMilliseconds() << std::endl;
+
+    
+    if (argc > 2 && argv[2] == "--save")
+    {
+        std::cout << "Saving to CSV..."  << std::endl;
+        grid_linear.save_csv("test_linear.csv");
+        grid_approx.save_csv("test_approx.csv");
+        grid_exact.save_csv("test_exact.csv");
+    }
+    else {
+        std::cout << "Not saving data."  << std::endl;
+    }
 
     std::cout << "Exiting program."  << std::endl;
     return 0;
